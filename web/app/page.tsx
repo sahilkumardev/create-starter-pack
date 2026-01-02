bc0224a6af5fe81a56c0f05b9417abd0324b71f8
sahilkumardev <sahilkumar710@outlook.com>
2026-01-02 11:22:28 +0530

    Add new icons and manifest for web application
    - Added apple-touch-icon.png for iOS devices.
    - Added favicon-16x16.png and favicon-32x32.png for browser tabs.
    - Created site.webmanifest for Progressive Web App support with specified icons and colors.

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,10 +1,5 @@
 import { HeroSection } from "@/components/hero-section";
-import * as React from "react";
 
 export default function Page() {
-  return (
-    <>
-      <HeroSection />
-    </>
-  );
+  return <HeroSection />;
 }
