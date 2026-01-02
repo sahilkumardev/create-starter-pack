ecf1506894ecc27d14882ab176ac43cb13a4acb4
sahilkumardev <sahilkumar710@outlook.com>
2026-01-02 07:53:14 +0530

    Refactor code structure for improved readability and maintainability

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 export default function Page() {
   return (
     <div className="h-screen flex justify-center items-center">
-      <h1>Home Page starter pack</h1>
+      <h1 className="font-mono">Home Page starter pack</h1>
     </div>
   );
 }
--- web/components/max-width-wrapper.tsx
+++ web/components/max-width-wrapper.tsx
@@ -1,13 +1,46 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
+import localFont from "next/font/local";
+
+const mono = localFont({
+  src: [
+    {
+      path: "../fonts/mono-light.woff2",
+      weight: "300",
+    },
+    {
+      path: "../fonts/mono-regular.woff2",
+      weight: "400",
+    },
+  ],
+  variable: "--font-mono",
+});
+
+const inter = localFont({
+  src: [
+    {
+      path: "../fonts/inter.woff2",
+    },
+  ],
+  variable: "--font-inter",
+  display: "swap",
+});
 
 export function MaxWidthWrapper({
   children,
   className,
-}: React.ComponentProps<"div">) {
+}: React.ComponentProps<"main">) {
   return (
-    <div className={cn("max-w-screen-2xl mx-auto relative", className)}>
+    <main
+      className={cn(
+        "max-w-screen-2xl mx-auto relative",
+        "font-inter antialiased tracking-wide font-normal leading-normal",
+        inter.variable,
+        mono.variable,
+        className
+      )}
+    >
       {children}
-    </div>
+    </main>
   );
 }
